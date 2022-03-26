0bf548815593ecc66e0e24ee6035e7b730882b50
Brian Kram <bkram@usca.edu>
2022-03-25 20:08:04 -0400

    CRUD professors

--- Test GUI/src/Database.java
+++ Test GUI/src/Database.java
@@ -25,11 +25,23 @@ public class Database
 	private final Connection connection;
 	private final PreparedStatement insertPerson;
 	private final PreparedStatement getPerson;
+	private final PreparedStatement updatePerson;
+	private final PreparedStatement deletePerson;
+
 	private final PreparedStatement getStudent;
+	// private final PreparedStatement deleteStudent;
+
 	private final PreparedStatement insertEmployee;
 	private final PreparedStatement getEmployee;
+	private final PreparedStatement updateEmployee;
+	private final PreparedStatement deleteEmployee;
+
+	private final PreparedStatement insertProfessor;
 	private final PreparedStatement getProfessor;
+	private final PreparedStatement deleteProfessor;
+
 	private final PreparedStatement getTA;
+
 	private final PreparedStatement getAdmin;
 	public Database(String dbPath) throws SQLException
 	{
@@ -39,19 +51,30 @@ public class Database
 		//allows the setup of a form connection
 		//this will also allow to present all the data on the form when searching 
 		this.insertPerson = this.connection.prepareStatement("""
-			INSERT INTO PERSON (first_name, last_name, birth_date)
-				VALUES (?, ?, ?)
-			RETURNING id""");
+			INSERT INTO PERSON (id, first_name, last_name, birth_date)
+				VALUES (?, ?, ?, ?)""");
 		this.getPerson = this.connection.prepareStatement("""
 				SELECT first_name, last_name, birth_date
 					FROM PERSON
 					WHERE id = ?""");
+		this.updatePerson = this.connection.prepareStatement("""
+			UPDATE PERSON 
+				SET
+					first_name = ?,
+					last_name = ?,
+					birth_date = ?
+				WHERE id = ?""");
+		this.deletePerson = this.connection.prepareStatement("""
+			DELETE FROM PERSON
+				WHERE id = ?""");
+		
 		this.getStudent = this.connection.prepareStatement("""
 			SELECT first_name, last_name, birth_date
 				FROM PERSON
 					INNER JOIN STUDENT
 						ON PERSON.id = STUDENT.id
 				WHERE STUDENT.id = ?""");
+
 		this.insertEmployee = this.connection.prepareStatement("""
 			INSERT INTO EMPLOYEE (person_id, department)
 				VALUES (?, ?)""");
@@ -61,6 +84,17 @@ public class Database
 					INNER JOIN PERSON
 						ON EMPLOYEE.person_id = PERSON.id
 				WHERE id = ?""");
+		this.updateEmployee = this.connection.prepareStatement("""
+			UPDATE EMPLOYEE 
+				SET department = ?
+				WHERE person_id = ?""");
+		this.deleteEmployee = this.connection.prepareStatement("""
+			DELETE FROM EMPLOYEE
+				WHERE person_id = ?""");
+
+		this.insertProfessor = this.connection.prepareStatement("""
+			INSERT INTO PROFESSOR (id)
+				VALUES (?)""");
 		this.getProfessor = this.connection.prepareStatement("""
 			SELECT first_name, last_name, birth_date, department
 				FROM PROFESSOR
@@ -69,6 +103,10 @@ public class Database
 					INNER JOIN PERSON
 						ON EMPLOYEE.person_id = PERSON.id
 				WHERE PROFESSOR.id = ?""");
+		this.deleteProfessor = this.connection.prepareStatement("""
+			DELETE FROM PROFESSOR
+				WHERE id = ?""");
+
 		this.getTA = this.connection.prepareStatement("""
 			SELECT first_name, last_name, birth_date, department
 				FROM TA
@@ -136,5 +174,73 @@ public class Database
 		getAdminRes.close();
 
 		return flags;
+	}
+
+	public void addProfessor(int id, String firstName, String lastName, String birthDate, String department) throws SQLException
+	{
+		this.connection.rollback();
+		this.insertPerson.setInt(1, id);
+		this.insertPerson.setString(2, firstName);
+		this.insertPerson.setString(3, lastName);
+		this.insertPerson.setString(4, birthDate);
+		this.insertPerson.executeUpdate();
+
+		this.insertEmployee.setInt(1, id);
+		this.insertEmployee.setString(2, department);
+		this.insertEmployee.executeUpdate();
+
+		this.insertProfessor.setInt(1, id);
+		this.insertProfessor.executeUpdate();
+
+		this.connection.commit();
+	}
+
+	public void updateProfessor(int id, String firstName, String lastName, String birthDate, String department) throws SQLException
+	{
+		this.connection.rollback();
+		this.updatePerson.setInt(1, id);
+		this.updatePerson.setString(2, firstName);
+		this.updatePerson.setString(3, lastName);
+		this.updatePerson.setString(4, birthDate);
+		this.updatePerson.executeUpdate();
+
+		this.updateEmployee.setInt(1, id);
+		this.updateEmployee.setString(2, department);
+		this.updateEmployee.executeUpdate();
+
+		this.connection.commit();
+	}
+
+	public Prof removeProfessor(int id) throws SQLException
+	{
+		this.connection.rollback();
+
+		this.getProfessor.setInt(1, id);
+		final var getProfRes = this.getProfessor.executeQuery();
+		if(!getProfRes.isBeforeFirst())
+		{
+			// Professor does not exist
+			throw new SQLException("Tried to remove professor, but professor does not exist");
+		}
+		getProfRes.next();
+		final var old = new Prof(
+			id,
+			getProfRes.getString("department"),
+			getProfRes.getString("first_name"),
+			getProfRes.getString("last_name"),
+			getProfRes.getString("birth_date"));
+		getProfRes.close();
+
+		this.deleteProfessor.setInt(1, id);
+		this.deleteProfessor.executeUpdate();
+		
+		this.deleteEmployee.setInt(1, id);
+		this.deleteEmployee.executeUpdate();
+		
+		this.deletePerson.setInt(1, id);
+		this.deletePerson.executeUpdate();
+
+		this.connection.commit();
+		return old;
 	}
 }
