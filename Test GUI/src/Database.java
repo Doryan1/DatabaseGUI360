c750b719074914e89c599460056a007cb62c2b02
Brian Kram <bkram@usca.edu>
2022-04-03 13:40:39 -0400

    Fix broken login

--- Test GUI/src/Database.java
+++ Test GUI/src/Database.java
@@ -187,7 +187,7 @@ public class Database
 	{
 		this.selectPerson.setInt(1, id);
 		final var getPersonRes = this.selectPerson.executeQuery();
-		if(getPersonRes.next())
+		if(!getPersonRes.next())
 		{
 			return Optional.empty();
 		}
